9153f3c36f54049ef43c69f2e432c7a079ed7445
Rajan Shukla <rajanmumbai6@gmail.com>
2026-07-30 16:09:21 +0530

    refactor: convert app layout to Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import PostDetail from "./pages/PostDetail";
+import PostDetail from "./pages/PostDetail";  
 import NotFound from "./pages/NotFound";
 
 const routes = [
@@ -19,9 +19,9 @@ const routes = [
 function App() {
   return (
     <BrowserRouter>
-      <div className="app">
+      <div className="flex min-h-screen flex-col">
         <Navbar />
-        <main className="app-main">
+        <main className="flex-1 px-6 py-6 md:px-8 md:py-10">
           <Routes>
             {routes.map((route) => (
               <Route key={route.path} path={route.path} element={route.element} />
